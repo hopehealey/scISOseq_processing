107d18b204f7524cb86aad5fb22503f92de998ec
hopehealey <hhealey@uoregon.edu>
2021-09-16 11:23:07 -0700

    added comments to the files to explain  the overall goals

--- custom_scripts/tama_associating_ensembl_ids_with_genes.py
+++ custom_scripts/tama_associating_ensembl_ids_with_genes.py
@@ -14,6 +14,7 @@ genes_w_multiple_ens_ids_for_one_g_id={}
 genes_w_multiple_ens_ids_for_one_g_id_locations = {}
 genes_w_multiple_ens_ids_for_one_g_id_found = {}
 
+#reading in gene names to get the corresponding ensembl id for each gene name as well as it's location
 with open (ens_g_names, "r") as names:
     for line in names:
         line=line.strip()
@@ -27,11 +28,18 @@ with open (ens_g_names, "r") as names:
         gname = moreline[0]
         tname = moreline[1]
 
+        #if the gene id has an associated ensembl id then save it in a dictionary for future use
         if len(moreline)>2:
             ensgname=moreline[2]
             enstname=moreline[3]
+            #if the gene id is not in the dictionary already then add it in
             if gname not in geneids:
                 geneids[gname]=[chro, start, end, gname, tname,ensgname, enstname]
+            #if the gene id is already in the dictionary (i.e. when there are multiple splice variants for one gene id) 
+            #then check if the ensembl id associated with that gene name is the same as the ensembl id associated with the current line
+            #if it is the same, then we don't need to change anything (this dictionary will just be used to associated ensembl names with gene ids)
+            #if it is different, then print out the potential conflict (there will be some cases of conflict)
+            #add the gene information to a dictionary which will be examined later and include the start and end sites, also adding in the same information from gene id that it originally matched to
             elif gname in geneids:
                 if geneids[gname][5]!= ensgname:
                     print("conflict", geneids[gname][5], geneids[gname][0], geneids[gname][1], geneids[gname][2], ensgname, chro, start, end)
@@ -56,6 +64,8 @@ c=0
 a=0
 transcripts=0
 
+#now opening up the real bed file and the bed file we are creating
+#reading through each line of the bed file 
 with open(gtf_name, "r") as gtf, open(new, "w") as newby:
     for line in gtf:
         line=line.strip()
@@ -66,11 +76,15 @@ with open(gtf_name, "r") as gtf, open(new, "w") as newby:
         gname = line[3].split(';')
         gname = gname[0]
         #print(gname)
+
+        #If the gene id is not in the odd cases (multiple ensembl ids for one gene id) and the gene id does have an ensembl id associated with it
+        #then proceed
+        #replace the gene id with the ensembl id 
+        #and print the line out to the new file
         if gname not in genes_w_multiple_ens_ids_for_one_g_id and gname in geneids:
             a+=1
             #pulling info [chro, start, end, gname, tname,ensgname, enstname]
             myinfo=geneids[gname]
-            #print("yes ma'am", gname, myinfo[5])
             #print(line)
             #print(re.sub('(gname)', 'myinfo[5]', safeline))
             name=gname
@@ -81,6 +95,14 @@ with open(gtf_name, "r") as gtf, open(new, "w") as newby:
             newby.write(s + "\n")
             #print("done")
 
+        #if there are multiple ensembl ids for the one gene id...
+        #try to find the ensembl id that is the closest distance to the particular transcript
+        #looking at both start and end sites and picking the ensembl id that is the closest for both
+        #however, we added in a second filter to prevent spurious naming
+        #we allowed only best matches within a certain distance of the original ensembl annotation to take on that id
+        #Because 3' ends of transcripts seem to be more variable with Iso-Seq data, we allowed a distance of 600bp from the end of the ensembl annotation
+        #Since there is less variability at the 5' ends, we only allowed 50bp of difference between the ensembl model and this model
+        #we then repeated writing out to the new file as done in the previous if statement
         elif gname in genes_w_multiple_ens_ids_for_one_g_id:
              c+=1
              #print(gname, line)
@@ -129,7 +151,8 @@ with open(gtf_name, "r") as gtf, open(new, "w") as newby:
                 newby.write(s + "\n")
              #print("NEWLINE", line)
 
-
+        #if the gene id does not have an ensembl id that matches to it (i.e. novel genes)
+        #then the line will stay the same 
         elif gname not in geneids or gname in genes_w_multiple_ens_ids_for_one_g_id:
             #print(safeline)
             c+=1
@@ -137,9 +160,9 @@ with open(gtf_name, "r") as gtf, open(new, "w") as newby:
             
 
 
-print("safe lines",a)
-print("problems lines+normal lines without gene ids", c)
-print("problem children", len(genes_w_multiple_ens_ids_for_one_g_id))
+print("lines that have an ensembl id associated with the gene id",a)
+print("genes_w_multiple_ens_ids_for_one_g_id_locations+normal lines without gene ids", c)
+print("genes_w_multiple_ens_ids_for_one_g_id_locations", len(genes_w_multiple_ens_ids_for_one_g_id))
 #print("transcripts saved", transcripts)
 
 #print(genes_w_multiple_ens_ids_for_one_g_id)
